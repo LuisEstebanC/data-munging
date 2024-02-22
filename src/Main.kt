5c36d352666d32281dfc6918dcefa62f9b07caca
LuisEstebanC <ing2.29capellan@gmail.com>
2024-02-22 18:29:37 -0400

    Adding columns

--- src/Main.kt
+++ src/Main.kt
@@ -3,7 +3,20 @@ import java.io.File
 
 fun main() {
     val file = File("C:\\Users\\ing29\\Downloads\\weather.dat")
-    val line = file.readLines();
+    val lines = file.readLines();
 
-    println(line)
+    var minunVariation = Double.MAX_VALUE;
+    var dayNumber =0;
+
+    for (line in lines){
+        val columns = line.trim().split(Regex("\\s+"))
+
+        println(columns)
+
+//        if (columns.size >= 3 && columns[0].toIntOrNull() != null){
+//            val maxT
+//        }
+
+
+    }
 }
