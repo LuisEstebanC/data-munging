e28bb0afc14b00a682b428c7b7aefb35ffe80f7e
LuisEstebanC <ing2.29capellan@gmail.com>
2024-02-22 18:45:02 -0400

    Solvin Weather problem

--- src/Main.kt
+++ src/Main.kt
@@ -5,18 +5,26 @@ fun main() {
     val file = File("C:\\Users\\ing29\\Downloads\\weather.dat")
     val lines = file.readLines();
 
-    var minunVariation = Double.MAX_VALUE;
+    var minimumVariation = Double.MAX_VALUE;
     var dayNumber =0;
 
     for (line in lines){
         val columns = line.trim().split(Regex("\\s+"))
 
-        println(columns)
+        if (columns.size >= 3 && columns[0].toIntOrNull() != null){
+            val maxT = columns[1].toDoubleOrNull()
+            val minT = columns[2].toDoubleOrNull()
 
-//        if (columns.size >= 3 && columns[0].toIntOrNull() != null){
-//            val maxT
-//        }
-
-
+            if (maxT !=null && minT !=null){
+                val variation = maxT - minT;
+                if (minimumVariation > variation){
+                    minimumVariation = variation
+                    dayNumber = columns[0].toInt();
+                }
+            }
+        }
     }
+
+   println("The minimum variation was $minimumVariation on day $dayNumber")
+
 }
